ef57b52fd8cadda17bba8f8a0975fab01645c545
alemica97 <alessandro.micalizzi08@gmail.com>
2022-03-29 18:48:58 +0200

    snack3

---
+++ snack3.php
@@ -0,0 +1,75 @@
+<!-- ## Snack 3
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato:
+DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni
+data con i relativi post. -->
+
+<?php
+    $array_date = [
+        '10/01/2019' => [
+            [
+                'title' => 'Post 1',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 1'
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
+        ],
+        '10/02/2019' => [
+            [
+                'title' => 'Post 3',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 3'
+            ]
+        ],
+        '15/05/2019' => [
+            [
+                'title' => 'Post 4',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 4'
+            ],
+            [
+                'title' => 'Post 5',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 5'
+            ],
+            [
+                'title' => 'Post 6',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 6'
+            ]
+        ],
+    ];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Snack3</title>
+</head>
+<body>
+    <?php
+
+        foreach($array_date as $x => $x_value){
+            ?>
+            <h3>Date <?php echo $x; ?></h3>
+            <?php
+            for($i = 0; $i < count($x_value); $i++){
+                foreach($x_value[$i] as $y => $y_value){
+                    ?>
+                    <p>•   <?php echo $y. ' -> ' .$y_value; ?></p>
+                    <?php
+                }
+
+                echo  '----------------';
+            }
+        }
+
+    ?>
+</body>
+</html>
